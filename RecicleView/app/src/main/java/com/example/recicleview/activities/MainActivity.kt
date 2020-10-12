7b15f5f769c0f9cc2c485a07a9ef10dc17ee9cf4
vinicius emanuel <vncemanuel@gmail.com>
2020-10-12 09:32:26 -0300

    flix layout

--- RecicleView/app/src/main/java/com/example/recicleview/activities/MainActivity.kt
+++ RecicleView/app/src/main/java/com/example/recicleview/activities/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.recicleview.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recicleview.R
+import com.example.recicleview.adapter.ClasseAdapter
 import com.example.recicleview.models.Classe
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //porque sim, já tava chato toda hora trocar
     private val self = this
+
+    private  lateinit var adapter: ClasseAdapter
 
     val classlist = mutableListOf(
         Classe("turma 1", 2019),
@@ -23,7 +32,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupRecyclerview() {
-
+        self.adapter = ClasseAdapter(self.classlist, self)
+        self.recycleView.adapter = self.adapter
+        self.recycleView.addItemDecoration(DividerItemDecoration(self, DividerItemDecoration.VERTICAL))
+        self.recycleView.layoutManager = LinearLayoutManager(self)
     }
 
 }
