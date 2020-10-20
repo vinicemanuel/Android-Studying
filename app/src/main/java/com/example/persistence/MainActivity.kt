e4daf23ac4533513d5ce8cfae133c49e07d6a9aa
vinicius emanuel <vncemanuel@gmail.com>
2020-10-20 20:35:33 -0300

    read and write local file

--- app/src/main/java/com/example/persistence/MainActivity.kt
+++ app/src/main/java/com/example/persistence/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.persistence
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
 
-    val internalFileName = "internalFile"
-    val externalFileName = "externalFile"
+    private val internalFileName = "internalFile.txt"
+    private val externalFileName = "externalFile.txt"
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -17,6 +22,9 @@ class MainActivity : AppCompatActivity() {
         this.readButton.setOnClickListener {
             val name = this.readInternal()
             val age = this.readExternal()
+
+            val result = "$name and $age"
+            this.resultText.text = result
         }
 
         this.writeButton.setOnClickListener {
@@ -26,15 +34,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun writeInternal(value: String) {
-
+        val path = this.filesDir
+        Log.d("main", path.toString())
+        this.openFileOutput(this.internalFileName, Context.MODE_PRIVATE).use {
+            it.write(value.toByteArray())
+        }
     }
 
     private fun readInternal(): String {
-        return ""
+
+        val fileInputStream = openFileInput(this.internalFileName)
+
+        val inputStreamReader: InputStreamReader = InputStreamReader(fileInputStream)
+        val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
+        val stringBuilder: StringBuilder = StringBuilder()
+        var text: String? = null
+        while ({ text = bufferedReader.readLine(); text }() != null) {
+            stringBuilder.append(text)
+        }
+
+        return stringBuilder.toString()
     }
 
     private fun writeExternal(value: String) {
-
+        val path = this.getExternalFilesDir(null)
     }
 
     private fun readExternal(): String {
