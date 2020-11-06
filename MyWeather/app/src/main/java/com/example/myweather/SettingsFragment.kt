59900fd70eef4dbdd81401c314d50ae13d25c427
vinicius emanuel <vncemanuel@gmail.com>
2020-11-05 21:36:32 -0300

    base shared preferences

--- MyWeather/app/src/main/java/com/example/myweather/SettingsFragment.kt
+++ MyWeather/app/src/main/java/com/example/myweather/SettingsFragment.kt
@@ -1,10 +1,15 @@
 package com.example.myweather
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 
 /**
  * A simple [Fragment] subclass.
@@ -14,15 +19,100 @@ import android.view.ViewGroup
 
 class SettingsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val TAG = "SettingsFragment"
+
+    private lateinit var saveButton: Button
+    private lateinit var rgTemperature: RadioGroup
+    private lateinit var rgLanguageRange: RadioGroup
+    private lateinit var rbTempC: RadioButton
+    private lateinit var rbTempF: RadioButton
+    private lateinit var rbLangPT: RadioButton
+    private lateinit var rbLangEN: RadioButton
+
+    private var temperatureUnit = "C º"
+    private var language = "PT"
+
+    private var prefs: SharedPreferences? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        this.configPrefs()
+        this.configData(view)
+        this.configTemperature(view)
+        this.configLanguage(view)
+        this.configSaveButton(view)
+    }
+
+    private fun configPrefs() {
+        this.prefs = context?.getSharedPreferences("my_weather_prefs", 0)
+    }
+
+    private fun configData(view: View) {
+        this.rbTempC = view.findViewById(R.id.rb_temperature_c)
+        this.rbTempF = view.findViewById(R.id.rb_temperature_f)
+
+        this.rbLangEN = view.findViewById(R.id.rb_language_en)
+        this.rbLangPT = view.findViewById(R.id.rb_language_pt)
+
+        val language = this.prefs?.getString("LANGUAGE","PT")
+        val temperatureUnit = this.prefs?.getString("TEMPERATURE_UNIT", "C º")
+
+        when (language) {
+            "EN" -> rbLangEN.isChecked = true
+            "PT" -> rbLangPT.isChecked = true
+        }
+
+        when (temperatureUnit) {
+            "C º" -> rbTempC.isChecked = true
+            "F º" -> rbTempF.isChecked = true
+        }
+    }
+
+    private fun configTemperature(view: View) {
+        this.rgTemperature = view.findViewById(R.id.rg_temperature_unit)
+        this.rgTemperature.setOnCheckedChangeListener { view, id ->
+            val radioButton = view.findViewById<RadioButton>(id)
+
+            if (radioButton.isChecked) {
+                when (radioButton.id) {
+                    R.id.rb_temperature_c -> this.temperatureUnit = "C º"
+                    R.id.rb_temperature_f -> this.temperatureUnit = "F º"
+                }
+            }
+        }
+    }
+
+    private fun configLanguage(view: View) {
+        this.rgLanguageRange = view.findViewById(R.id.rg_language)
+        this.rgLanguageRange.setOnCheckedChangeListener { view, id ->
+            val radioButton = view.findViewById<RadioButton>(id)
+
+            if (radioButton.isChecked) {
+                when (radioButton.id) {
+                    R.id.rb_language_en -> this.language = "EN"
+                    R.id.rb_language_pt -> this.language = "PT"
+                }
+            }
+        }
+    }
+
+    private fun configSaveButton(view: View) {
+        this.saveButton = view.findViewById(R.id.btn_save)
+        this.saveButton.setOnClickListener {
+            val editor = this.prefs?.edit()
+
+            editor?.apply {
+                putString("TEMPERATURE_UNIT", temperatureUnit)
+                putString("LANGUAGE", language)
+            }
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 }
