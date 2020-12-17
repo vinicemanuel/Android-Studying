40759e947aaf4a651913aefd206338dee2e11b2c
vinicius emanuel <vncemanuel@gmail.com>
2020-12-17 19:16:36 -0300

    using LocalBroadcastManager

--- avançado/Broadcas/app/src/main/java/com/example/broadcasexercice/MainActivity.kt
+++ avançado/Broadcas/app/src/main/java/com/example/broadcasexercice/MainActivity.kt
@@ -1,30 +1,67 @@
 package com.example.broadcasexercice
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val receiver: BroadcastReceiver = InternalBroadcast()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         buttonSDKLass26.setOnClickListener {
-            val intent = Intent(this, RebootBroadcast::class.java).also {
+            val intent = Intent(this, ManifestBroadcast::class.java).also {
                 it.action = "MY_CUSTOM_ACTION_ON_MANIFEST"
                 sendBroadcast(it)
-                LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+
             }
         }
 
         buttonSDKGreaterOrEqual26.setOnClickListener {
-
+            val intent = Intent("MY_CUSTOM_ACTION_ON_INTERNEAL_BROADCAST").also {
+                LocalBroadcastManager.getInstance(this).sendBroadcast(it)
+            }
         }
 
         buttonOpenApp.setOnClickListener {
 
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, IntentFilter("MY_CUSTOM_ACTION_ON_INTERNEAL_BROADCAST"))
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver)
+    }
+
+    inner class InternalBroadcast: BroadcastReceiver() {
+
+        val TAG = "MainActivity - InternalBroadcast"
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+
+            Log.d(TAG, "onReceive chamado")
+
+            if (intent?.action == "MY_CUSTOM_ACTION_ON_INTERNEAL_BROADCAST") {
+                Log.d(TAG, "Chamada registrada em uma activity")
+            }
         }
     }
 }
--- avançado/Broadcas/app/src/main/java/com/example/broadcasexercice/ManifestBroadcast.kt
+++ avançado/Broadcas/app/src/main/java/com/example/broadcasexercice/ManifestBroadcast.kt
@@ -5,7 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 
-class RebootBroadcast: BroadcastReceiver() {
+class ManifestBroadcast: BroadcastReceiver() {
 
     val TAG = "RebootBroadcast"
 
